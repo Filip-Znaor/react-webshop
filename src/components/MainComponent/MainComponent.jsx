fe86e4bb41b53f8558f5fac993db4312c80c7f19
Filip-Znaor <filip1309@gmail.com>
2024-03-11 19:35:49 +0100

    update

---
+++ src/components/MainComponent/MainComponent.jsx
@@ -0,0 +1,9 @@
+function MainComponent() {
+    return (
+    <div>    
+        <h2> Main Component part</h2>
+        <p>Here will be the list of products or single product page</p>
+    </div>)
+}
+
+export {MainComponent}
